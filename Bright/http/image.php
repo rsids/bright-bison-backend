8cee5685f50883792a076374f62b2da0eed766bd
Ids Klijnsma <werk@idsklijnsma.nl>
2016-11-19 14:27:05 +0100

    Update image script

--- Bright/http/image.php
+++ Bright/http/image.php
@@ -2,60 +2,84 @@
 include_once(__DIR__  . '/../Bright.php');
 
 if(!isset($_GET['mode']) || !isset($_GET['src'])) {
-	// redirect to default image
-	header('HTTP/1.1 500 Internal Server Error');
-	header('Location: ' . ERROR_IMG);
-	exit;
+    // redirect to default image
+    header('HTTP/1.1 500 Internal Server Error');
+    header('Location: ' . ERROR_IMG);
+    exit;
 } else {
-	$mode = addslashes($_GET['mode']);
-	$src = $_GET['src'];
-	// Unset all other get parameters
-	$debug = -1;
-	if(!LIVESERVER && isset($_GET['phpThumbDebug'])) {
-		$debug = (int)$_GET['phpThumbDebug'];
-	}
+    $mode = addslashes($_GET['mode']);
+    $src = $_GET['src'];
+    // Unset all other get parameters
+    $debug = -1;
+    if(!LIVESERVER && isset($_GET['phpThumbDebug'])) {
+        $debug = (int)$_GET['phpThumbDebug'];
+    }
 
-	$original = filter_input_array(INPUT_GET);
+    $original = filter_input_array(INPUT_GET);
 
-	$_GET = array();
-	$_GET['src'] = $src;
-	
-	$modes = unserialize(IMAGE_MODES);
-	$modes['brightthumb'] = array('w' => 98, 'h' => 74);
-	$modes['brightlogo'] = array('w' => 117, 'h' => 117, 'far' => 'C','f'=> 'png', 'bg'=>'0000FF');
-	if(array_key_exists($mode, $modes)) {
+    $_GET = array();
+    $_GET['src'] = $src;
 
-		$ext = strtolower(substr($original['src'], -3));
-		if(!array_key_exists('f', $modes[$mode])) {
-			switch($ext){
-				case 'png':case 'gif': 
-					$_GET['f'] = $ext;
-					break;
-				case 'peg': case 'jpg':
-					$_GET['f'] = 'jpeg';
-					break;
-			}
-		}
-		foreach($modes[$mode] as $key => $value) {
-			$_GET[$key] = $value;
-		}
-		if($debug > -1)
-			$_GET['phpThumbDebug'] = $debug;
+    $modes = unserialize(IMAGE_MODES);
+    $modes['brightthumb'] = array('w' => 98, 'h' => 74);
+    $modes['brightlogo'] = array('w' => 117, 'h' => 117, 'far' => 'C','f'=> 'png', 'bg'=>'0000FF');
+    if(array_key_exists($mode, $modes)) {
 
-		if(array_key_exists('allowedvars', $modes[$mode])) {
-			foreach($modes[$mode]['allowedvars'] as $var) {
-				if(array_key_exists($var, $original)) {
-					$_GET[$var] = $original[$var];
-				}
-			}
-			unset($_GET['allowedvars']);
-		}
+        $ext = strtolower(substr($original['src'], -3));
+        if(!array_key_exists('f', $modes[$mode])) {
+            switch($ext){
+                case 'png':
+                case 'gif':
+                    $_GET['f'] = $ext;
+                    break;
+                case 'peg':
+                case 'jpg':
+                    $_GET['f'] = 'jpeg';
+                    break;
+                default:
+                    // Invalid image
+                    header('HTTP/1.1 500 Internal Server Error');
+                    header('Location: ' . ERROR_IMG);
+                    exit;
+            }
+        }
+        if($mode === 'brightthumb') {
+            $img = @exif_thumbnail(BASEPATH . $_GET['src'], $modes['brightthumb']['w'] , $modes['brightthumb']['h'], $type);
+            if($img !== false) {
+                header('Content-type: ' .image_type_to_mime_type($type));
+                echo $img;
+                exit;
+            }
+        }
 
-		include(BASEPATH . 'bright/library/phpThumb/phpThumb.php');
-	} else {
-		header('HTTP/1.1 500 Internal Server Error');
-		header('Location: ' . ERROR_IMG);
-		exit;
-	}
+        foreach($modes[$mode] as $key => $value) {
+            $_GET[$key] = $value;
+        }
+        if($debug > -1)
+            $_GET['phpThumbDebug'] = $debug;
+
+        if(array_key_exists('allowedvars', $modes[$mode])) {
+            foreach($modes[$mode]['allowedvars'] as $var) {
+                if(array_key_exists($var, $original)) {
+                    $_GET[$var] = $original[$var];
+                }
+            }
+            unset($_GET['allowedvars']);
+        }
+        ob_start();
+        include(BASEPATH . 'bright/library/phpThumb/phpThumb.php');
+
+        $img = ob_get_clean();
+
+        if(!$img) {
+            // Output original image
+            header('Content-type: image/' .$_GET['f']);
+            echo file_get_contents(BASEPATH . $_GET['src']);
+        }
+    } else {
+        header('HTTP/1.1 500 Internal Server Error');
+        header('Location: ' . ERROR_IMG);
+        exit;
+    }
 
 }
